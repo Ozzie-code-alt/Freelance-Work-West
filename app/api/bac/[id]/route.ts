f7d8b6a5a1c0b970c932fe57d7a3a64a7a4e0dbc
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-03-03 20:16:03 +0800

    FixedBAC OpenMOre

--- app/api/bac/[id]/route.ts
+++ app/api/bac/[id]/route.ts
@@ -1,10 +1,11 @@
 
 import { connectMongo } from "@/lib/connection";
 import BAC from "@/models/bac";
+import { NextApiRequest } from "next";
 import { NextResponse } from "next/server"
 
-export async function GET({params}:any){
-    const {id} = params // we grab id from link - desctructure
+export async function GET(req:NextApiRequest, {params}:any){
+    const {id} = params //we grab id from link - desctructure
     await connectMongo();
    const bacInfo = await BAC.findById({_id:id})
    return NextResponse.json({bacInfo}, {status:200})
