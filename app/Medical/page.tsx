143b369be09d997f13ed6cbcda71e7628b307ca0
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-03-07 21:15:11 +0800

    Update code formatting and fix minor bugs

---
+++ app/Medical/page.tsx
@@ -0,0 +1,12 @@
+
+import LoginModal from "@/components/LoginModal";
+
+const BACpage = () => {
+  return (
+    <section>
+      <LoginModal route="/Cashier/CashierPersonalForm"/>
+    </section>
+  );
+};
+
+export default BACpage;
---
+++ app/api/medical/route.ts
@@ -0,0 +1,25 @@
+import { connectMongo } from "@/lib/connection";
+import Medical from "@/models/medical";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+
+
+export async function POST(req:NextApiRequest){
+    try {
+        //@ts-ignore
+        const {date, userName, servicesReceived, externalClient, pointOfOrigin, officeVisited, internalClient, sex, reliability,responsiveness,access,communication,costs,integrity,assurance,outcome, message} = await req.json(); 
+        await connectMongo();
+        await Medical.create({date, userName, servicesReceived, externalClient, pointOfOrigin, officeVisited, internalClient, sex ,reliability,responsiveness,access,communication,costs,integrity,assurance,outcome, message})
+        console.log("Medical Information Sent")
+        return NextResponse.json({message:"Medical Information Sent"}, {status:201})
+    } catch (error) {
+        return NextResponse.json({message:"An Error Occured While Sending Medical Informaton"},{status:500})
+    }
+}
+
+export async function GET(){
+        await connectMongo()
+            const medicals = await Medical.find()
+            console.log("Logg BAC Information",medicals)
+            return NextResponse.json({medicals}) 
+}
