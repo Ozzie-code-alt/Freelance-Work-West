d7f84c9cb8374423a45d556859f7000cc519bb4e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-03-07 19:52:03 +0800

    Refactor GET function to include NextApiRequest parameter

--- app/api/accounting/[id]/route.ts
+++ app/api/accounting/[id]/route.ts
@@ -2,8 +2,9 @@
 import { connectMongo } from "@/lib/connection";
 import Accounting from "@/models/accounting";
 import { NextResponse } from "next/server"
+import { NextApiRequest } from "next";
 
-export async function GET({params}:any){
+export async function GET(req:NextApiRequest, {params}:any){
     const {id} = params // we grab id from link - desctructure
     await connectMongo();
    const accountingInfo = await Accounting.findById({_id:id})
