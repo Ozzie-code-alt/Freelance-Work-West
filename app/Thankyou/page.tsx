d784fa9db9814153748d39257209229189923fc0
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-10 19:21:27 +0800

    fixed Vercel Error

--- app/Thankyou/page.tsx
+++ app/Thankyou/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      Thank you For Fililng up the  form "message placeholder"
+      Thank you For Fililng up the  form &apos;message placeholder&apos;
     </div>
   );
 };
