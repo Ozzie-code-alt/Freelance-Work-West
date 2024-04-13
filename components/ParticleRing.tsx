48385c4823e27a2274caf0a54c0d5eedc6836188
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-13 21:34:15 +0800

    Changes in Login Page

--- components/ParticleRing.tsx
+++ components/ParticleRing.tsx
@@ -1,9 +1,13 @@
 "use client"
 import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
 import { OrbitControls, Sphere } from "@react-three/drei";
 import { pointsInner, pointsOuter } from "@/lib/ParticleRingUtils";
+import { DeviceOrientationControls } from "@react-three/drei";
 import { Group } from "three";
+
+
+extend({ DeviceOrientationControls });
 
 const ParticleRing = () => {
   return (
@@ -15,15 +19,12 @@ const ParticleRing = () => {
         style={{ height: "100vh" }}
         className="bg-slate-900"
       >
-        <OrbitControls maxDistance={20} minDistance={10} />
         <directionalLight />
         <pointLight position={[-30, 0, -30]} power={10.0} />
+        <DeviceOrientationControls/>
         <PointCircle />
       </Canvas>
 
-      <h1 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-slate-200 font-medium text-2xl md:text-5xl pointer-events-none">
-        Drag & Zoom
-      </h1>
     </div>
   );
 };
