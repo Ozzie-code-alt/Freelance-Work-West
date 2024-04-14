4ebeadf6537e4246a0af7ecb8c09366021ec7263
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-14 19:32:42 +0800

    Changed Thank you Page

--- app/Thankyou/page.tsx
+++ app/Thankyou/page.tsx
@@ -1,11 +1,25 @@
-import React from "react";
 
-const page = () => {
+"use client";
+import React from "react";
+import { SparklesCore } from "@/components/ui/errorSparkles";
+ 
+export default function SparklesPreview() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      Thank you For Fililng up the  form &apos;message placeholder&apos;
+    <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+       Thank you for your feedback
+      </h1>
     </div>
   );
-};
-
-export default page;
+}
