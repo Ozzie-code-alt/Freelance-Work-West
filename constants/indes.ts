b8e54e75b48bcea448931de9cd0cbdfdd2a6381b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-13 21:32:38 +0800

    feat: add filter by month

--- constants/indes.ts
+++ constants/indes.ts
@@ -64,3 +64,19 @@ export const Offices = [
     route:"/AdminContainer/AdministrationOfficesandFinance"
 },
 ]
+
+
+export const Months = [
+    { title: "January" },
+    { title: "February" },
+    { title: "March" },
+    { title: "April" },
+    { title: "May" },
+    { title: "June" },
+    { title: "July" },
+    { title: "August" },
+    { title: "September" },
+    { title: "October" },
+    { title: "November" },
+    { title: "December" }
+];
