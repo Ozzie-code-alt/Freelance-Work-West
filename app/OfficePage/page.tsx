837498a11f845ca4201377ab4b2905454ae85bbb
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-08-24 00:05:29 +0800

    refactor: Office page layout

--- app/OfficePage/page.tsx
+++ app/OfficePage/page.tsx
@@ -7,7 +7,7 @@ const Officepage = () => {
     <section className='w-screen h-screen'>
       <div className='flex flex-col md:flex-row '>
         {/*Left Div */}
-        <div className='h-screen px-20 py-10 w-auto border-r-2 '>
+        <div className='h-screen bg-slate-500/10 px-20 py-10 w-auto border-r-2 '>
           <div className='flex flex-col h-full '>
             <div className='pb-5'>
               <h1>
@@ -18,11 +18,10 @@ const Officepage = () => {
             <UserInfo />
           </div>
         </div>
-        <div className='w-full h-full flex  flex-col justify-center gap-20 items-center'>
-          <div className=' px-10 py-10'>
-            <h1 className='text-3xl md:text-5xl text-center leading-[60px]'>
-              <span className='font-bold'>West Visayas State</span>
-              <br /> Feedback Forms
+        <div className='w-full h-full flex  flex-col justify-center items-center'>
+          <div className='px-10 pt-3'>
+            <h1 className='text-3xl md:text-5xl text-center '>
+              <span className='font-bold'>West Visayas State</span> Feedback Forms
             </h1>
           </div>
 
