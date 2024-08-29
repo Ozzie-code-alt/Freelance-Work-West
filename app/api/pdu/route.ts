cd9c64054ae60cd1f36992289eacd3a7fba85a6b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-08-29 23:49:25 +0800

    feat: PDU and RMU Form Page and Serverside api payload

---
+++ app/api/pdu/route.ts
@@ -0,0 +1,75 @@
+import { connectMongo } from '@/lib/connection';
+import PDU from '@/models/pdu';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: NextApiRequest) {
+  try {
+    const {
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      cc1,
+      cc2,
+      cc3,
+      sex,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean
+      //@ts-ignore
+    } = await req.json();
+    await connectMongo();
+    await PDU.create({
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      sex,
+      cc1,
+      cc2,
+      cc3,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean
+    });
+    console.log('PDU Information Sent');
+    return NextResponse.json({ message: 'PDU Information Sent it work now' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An Error Occured While Sending Personal Informaton' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  await connectMongo();
+  const pdus = await PDU.find();
+  console.log('Log pdu Information', pdus);
+  const dateConverted = pdus.map((pdu) => ({
+    ...pdu._doc,
+    date: pdu.date.toISOString().split('T')[0]
+  }));
+  return NextResponse.json({ pdus: dateConverted }, { status: 200 });
+}
---
+++ app/api/rmu/route.ts
@@ -0,0 +1,75 @@
+import { connectMongo } from '@/lib/connection';
+import RMU from '@/models/rmu';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: NextApiRequest) {
+  try {
+    const {
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      cc1,
+      cc2,
+      cc3,
+      sex,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean
+      //@ts-ignore
+    } = await req.json();
+    await connectMongo();
+    await RMU.create({
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      sex,
+      cc1,
+      cc2,
+      cc3,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean
+    });
+    console.log('RMU Information Sent');
+    return NextResponse.json({ message: 'RMU Information Sent it work now' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An Error Occured While Sending Personal Informaton' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  await connectMongo();
+  const rmus = await RMU.find();
+  console.log('Logg RMU Information', rmus);
+  const dateConverted = rmus.map((rmu) => ({
+    ...rmu._doc,
+    date: rmu.date.toISOString().split('T')[0]
+  }));
+  return NextResponse.json({ rmus: dateConverted }, { status: 200 });
+}
