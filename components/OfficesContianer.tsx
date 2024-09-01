9e46e021787ac5739f42283a465818e8b1b1e005
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-01 16:23:13 +0800

    refactor: remove name coulumns in admin data, remove dummy content

--- components/OfficesContianer.tsx
+++ components/OfficesContianer.tsx
@@ -14,6 +14,11 @@ import IndustrialTechFormPage from './IndustrialTech/IndustrialTechForm';
 import SBMFormPage from './SBM/SBMForm';
 import RegistrarFormPage from './Registrar/RegistrarForm';
 import OSAFormPage from './OSA/OSAForm';
+import PDUFormPage from './PDU/PDUForm';
+import RMUFormPage from './RMU/RMUForm';
+import RIXFormPage from './RIX/RIXForm';
+import SportsOfficeFormPage from './SportsOffice/SportsOfficeForm';
+import SupplyBuildingFormPage from './SupplyBuilding/SupplyBuildingForm';
 const OfficesContianer = () => {
   const cards = data.map((card, index) => <Card key={card.src} card={card} index={index} />);
   return <Carousel items={cards} />;
@@ -21,36 +26,6 @@ const OfficesContianer = () => {
 
 export default OfficesContianer;
 
-const DummyContent = () => {
-  return (
-    <>
-      {[...new Array(3).fill(1)].map((_, index) => {
-        return (
-          <div
-            key={'dummy-content' + index}
-            className='bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4'
-          >
-            <p className='text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto'>
-              <span className='font-bold text-neutral-700 dark:text-neutral-200'>
-                The first rule of Apple club is that you boast about Apple club.
-              </span>{' '}
-              Keep a journal, quickly jot down a grocery list, and take amazing class notes. Want to
-              convert those notes to text? No problem. Langotiya jeetu ka mara hua yaar is ready to
-              capture every thought.
-            </p>
-            <Image
-              src='https://assets.aceternity.com/macbook.png'
-              alt='Macbook mockup from Aceternity UI'
-              height='500'
-              width='500'
-              className='md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain'
-            />
-          </div>
-        );
-      })}
-    </>
-  );
-};
 const data = [
   {
     category: 'Admin Office and Finance Office',
@@ -123,30 +98,30 @@ const data = [
     category: 'Planning and Development Unit Office',
     title: 'Planning and Development Unit',
     src: '/Offices/PDU.JPG',
-    content: <DummyContent />
+    content: <PDUFormPage/>
   },
   {
     category: 'Records and Management Unit Office',
     title: 'Records and Management Unit',
     src: '/Offices/RMU.JPG',
-    content: <DummyContent />
+    content: <RMUFormPage/>
   },
   {
     category: 'RIX Office',
     title: 'RIX',
     src: 'https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    content: <DummyContent />
+    content: <RIXFormPage/>
   },
   {
     category: 'Sports Office ',
     title: 'Sports Office',
     src: 'https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    content: <DummyContent />
+    content: <SportsOfficeFormPage/>
   },
   {
     category: 'Supply Building 1 Office',
     title: 'Supply Building 1',
     src: 'https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    content: <DummyContent />
+    content: <SupplyBuildingFormPage/>
   }
 ];
