8008d8417cd6ad94d27e73b882f7c3ceff77554e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-26 22:38:44 +0800

    refactor: minot changes

--- components/AffairsOffice/AffairsOffice.tsx
+++ components/AffairsOffice/AffairsOffice.tsx
@@ -148,14 +148,14 @@ export const AffairsOfficeFormModal = ({ isOpen, setIsOpen, adminProps }: any) =
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           onClick={() => setIsOpen(false)}
-          className='bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center  cursor-pointer'
+          className='bg-slate-900/20 backdrop-blur p-8 fixed  overflow-y-scroll inset-0 z-50 grid place-items-center  cursor-pointer'
         >
           <motion.div
             initial={{ scale: 0, rotate: '12.5deg' }}
             animate={{ scale: 1, rotate: '0deg' }}
             exit={{ scale: 0, rotate: '0deg' }}
             onClick={(e) => e.stopPropagation()}
-            className='bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full  h-fit shadow-xl cursor-default relative '
+            className='bg-gradient-to-br from-violet-600 to-indigo-600 text-white  p-6 rounded-lg w-full  h-fit shadow-xl cursor-default relative '
           >
             <FiAlertCircle className='text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24' />
 
--- components/AffairsOffice/AffairsOfficeCC.tsx
+++ components/AffairsOffice/AffairsOfficeCC.tsx
@@ -86,16 +86,16 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           onClick={() => setIsOpen(false)}
-          className='bg-slate-900/20 backdrop-blur p-20 fixed inset-0 z-50 grid place-items-center cursor-pointer'
+          className='bg-slate-900/20 backdrop-blur p-4 sm:p-8 md:p-20 fixed inset-0 z-50 grid place-items-center  cursor-pointer'
         >
           <motion.div
             initial={{ scale: 0, rotate: '12.5deg' }}
             animate={{ scale: 1, rotate: '0deg' }}
             exit={{ scale: 0, rotate: '0deg' }}
             onClick={(e) => e.stopPropagation()}
-            className='bg-gradient-to-br from-violet-600 to-indigo-600 mb-60 text-white p-6 rounded-lg w-full  h-auto shadow-xl cursor-default relative '
+            className='bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-4 sm:p-6 rounded-lg w-full max-w-[800px]  h-auto shadow-xl cursor-default relative'
           >
-            <FiAlertCircle className='text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24' />
+            <FiAlertCircle className='text-white/10 rotate-12 text-[150px] sm:text-[250px] absolute z-0 -top-10 sm:-top-24 -left-10 sm:-left-24' />
 
             <Form {...form}>
               <form
@@ -106,8 +106,8 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                   control={form.control}
                   name='cc1'
                   render={({ field }) => (
-                    <FormItem className='flex flex-col gap-5'>
-                      <FormLabel className='text-5xl'>
+                    <FormItem className='flex flex-col gap-2 sm:gap-5 '>
+                      <FormLabel className='text-lg sm:text-5xl'>
                         {' '}
                         Which of The Follow best Descibes Your awareness of a CC ?
                       </FormLabel>
@@ -118,7 +118,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                               variant='outline'
                               role='combobox'
                               className={cn(
-                                'w-fit justify-between ',
+                                'w-full max-w-[180px] justify-between text-sm overflow-hidden',
                                 !field.value && 'bg-white text-black'
                               )}
                             >
@@ -156,11 +156,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                           </Command>
                         </PopoverContent>
                       </Popover>
-                      {/* <FormDescription>
-                        <p className='text-yellow-500'>
-                          This is the language that will be used in the dashboard.
-                        </p>
-                      </FormDescription> */}
+  
                       <FormMessage />
                     </FormItem>
                   )}
@@ -170,7 +166,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                   name='cc2'
                   render={({ field }) => (
                     <FormItem className='flex flex-col gap-5 '>
-                      <FormLabel className='text-5xl'>
+                      <FormLabel className='text-lg sm:text-5xl'>
                         {' '}
                         <p> if aware of CC, would you say that the CC of this office was ?</p>
                       </FormLabel>
@@ -181,7 +177,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                               variant='outline'
                               role='combobox'
                               className={cn(
-                                'w-fit justify-between',
+                                'w-full max-w-[180px] justify-between text-sm',
                                 !field.value && 'bg-white text-black'
                               )}
                             >
@@ -228,7 +224,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                   name='cc3'
                   render={({ field }) => (
                     <FormItem className='flex flex-col gap-5'>
-                      <FormLabel className='text-5xl'>
+                      <FormLabel className='text-lg sm:text-5xl'>
                         If aware of CC, how much did the CC help you in your transaction ?
                       </FormLabel>
                       <Popover>
@@ -238,7 +234,7 @@ export const AffairsOfficeCC = ({ isOpen, setIsOpen, adminProps }: any) => {
                               variant='outline'
                               role='combobox'
                               className={cn(
-                                'w-fit justify-between',
+                                'w-full max-w-[180px] justify-between text-sm',
                                 !field.value && 'bg-white text-black'
                               )}
                             >
