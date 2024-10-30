cd4bce1e1b987f09d307db09bc6612009b2b58a5
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-30 15:56:10 +0800

    feat: refactor public affair api

--- app/api/publicAffairs/[id]/route.ts
+++ app/api/publicAffairs/[id]/route.ts
@@ -6,24 +6,24 @@ import { NextResponse } from 'next/server';
 export async function GET(req: NextApiRequest, { params }: any) {
   const { id } = params; //we grab id from link - desctructure
   await connectMongo();
-  const publicaffairInfoResponse = await PublicAffairs.findById({ _id: id });
+  const publicAffairInfoResponse = await PublicAffairs.findById({ _id: id });
 
-  const publicaffairInfo = {
-    userName: publicaffairInfoResponse.userName,
-    sex: publicaffairInfoResponse.sex,
-    cc1: publicaffairInfoResponse.cc1,
-    cc2: publicaffairInfoResponse.cc2,
-    cc3: publicaffairInfoResponse.cc3,
-    sqd0: publicaffairInfoResponse.sqd0,
-    sqd1: publicaffairInfoResponse.sqd1,
-    sqd2: publicaffairInfoResponse.sqd2,
-    sqd3: publicaffairInfoResponse.sqd3,
-    sqd4: publicaffairInfoResponse.sqd4,
-    sqd5: publicaffairInfoResponse.sqd5,
-    sqd6: publicaffairInfoResponse.sqd6,
-    sqd7: publicaffairInfoResponse.sqd7,
-    sqd8: publicaffairInfoResponse.sqd8,
-    mean: publicaffairInfoResponse.mean
+  const publicAffairInfo = {
+    userName: publicAffairInfoResponse.userName,
+    sex: publicAffairInfoResponse.sex,
+    cc1: publicAffairInfoResponse.cc1,
+    cc2: publicAffairInfoResponse.cc2,
+    cc3: publicAffairInfoResponse.cc3,
+    sqd0: publicAffairInfoResponse.sqd0,
+    sqd1: publicAffairInfoResponse.sqd1,
+    sqd2: publicAffairInfoResponse.sqd2,
+    sqd3: publicAffairInfoResponse.sqd3,
+    sqd4: publicAffairInfoResponse.sqd4,
+    sqd5: publicAffairInfoResponse.sqd5,
+    sqd6: publicAffairInfoResponse.sqd6,
+    sqd7: publicAffairInfoResponse.sqd7,
+    sqd8: publicAffairInfoResponse.sqd8,
+    mean: publicAffairInfoResponse.mean
   };
-  return NextResponse.json({ publicaffairInfo }, { status: 200 });
+  return NextResponse.json({ publicAffairInfo }, { status: 200 });
 }
