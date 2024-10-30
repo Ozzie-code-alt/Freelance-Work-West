2cf13bd29084aeffdfa10a5d1a8d21bac4d84f18
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-30 15:39:42 +0800

    feat: refactor cultutal and campus admin  api and models

--- app/api/campusAdmin/[id]/route.ts
+++ app/api/campusAdmin/[id]/route.ts
@@ -1,29 +1,29 @@
 import { connectMongo } from '@/lib/connection';
-import BAC from '@/models/bac';
+import CampusAdmin from '@/models/campusAdmin';
 import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: NextApiRequest, { params }: any) {
   const { id } = params; //we grab id from link - desctructure
   await connectMongo();
-  const bacInfoResponse = await BAC.findById({ _id: id });
+  const campusAdminInfoResponse = await CampusAdmin.findById({ _id: id });
 
-  const bacInfo = {
-    userName: bacInfoResponse.userName,
-    sex: bacInfoResponse.sex,
-    cc1: bacInfoResponse.cc1,
-    cc2: bacInfoResponse.cc2,
-    cc3: bacInfoResponse.cc3,
-    sqd0: bacInfoResponse.sqd0,
-    sqd1: bacInfoResponse.sqd1,
-    sqd2: bacInfoResponse.sqd2,
-    sqd3: bacInfoResponse.sqd3,
-    sqd4: bacInfoResponse.sqd4,
-    sqd5: bacInfoResponse.sqd5,
-    sqd6: bacInfoResponse.sqd6,
-    sqd7: bacInfoResponse.sqd7,
-    sqd8: bacInfoResponse.sqd8,
-    mean: bacInfoResponse.mean
+  const campusAdminInfo = {
+    userName: campusAdminInfoResponse.userName,
+    sex: campusAdminInfoResponse.sex,
+    cc1: campusAdminInfoResponse.cc1,
+    cc2: campusAdminInfoResponse.cc2,
+    cc3: campusAdminInfoResponse.cc3,
+    sqd0: campusAdminInfoResponse.sqd0,
+    sqd1: campusAdminInfoResponse.sqd1,
+    sqd2: campusAdminInfoResponse.sqd2,
+    sqd3: campusAdminInfoResponse.sqd3,
+    sqd4: campusAdminInfoResponse.sqd4,
+    sqd5: campusAdminInfoResponse.sqd5,
+    sqd6: campusAdminInfoResponse.sqd6,
+    sqd7: campusAdminInfoResponse.sqd7,
+    sqd8: campusAdminInfoResponse.sqd8,
+    mean: campusAdminInfoResponse.mean
   };
-  return NextResponse.json({ bacInfo }, { status: 200 });
+  return NextResponse.json({ campusAdminInfo }, { status: 200 });
 }
