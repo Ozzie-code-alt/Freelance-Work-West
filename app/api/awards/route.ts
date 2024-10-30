b5e81a7d370f520ce3b70554697a8bc62e9650f0
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-30 15:33:06 +0800

    feat: add awards api

---
+++ app/api/awards/route.ts
@@ -0,0 +1,77 @@
+import { connectMongo } from '@/lib/connection';
+import Awards from '@/models/awards';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: NextApiRequest) {
+  try {
+    const {
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      cc1,
+      cc2,
+      cc3,
+      sex,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean,
+      comments
+      //@ts-ignore
+    } = await req.json();
+    await connectMongo();
+    await Awards.create({
+      date,
+      userName,
+      servicesReceived,
+      clientType,
+      pointOfOrigin,
+      officeVisited,
+      age,
+      sex,
+      cc1,
+      cc2,
+      cc3,
+      sqd0,
+      sqd1,
+      sqd2,
+      sqd3,
+      sqd4,
+      sqd5,
+      sqd6,
+      sqd7,
+      sqd8,
+      mean,
+      comments
+    });
+    console.log('Awards Information Sent');
+    return NextResponse.json({ message: 'Awards Information Sent it work now' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An Error Occured While Sending Personal Informaton' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  await connectMongo();
+  const awards = await Awards.find();
+  console.log('Logg Awards Information', awards);
+  const dateConverted = awards.map((award) => ({
+    ...award._doc,
+    date: award.date.toISOString().split('T')[0]
+  }));
+  return NextResponse.json({ awards: dateConverted }, { status: 200 });
+}
