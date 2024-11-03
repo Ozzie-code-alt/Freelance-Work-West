88be167460232c59733b2c03574a68f265a8a5ca
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-11-03 18:07:34 +0800

    feat: add Student Affairs admin

--- app/AdminContainer/BACAdmin copy/page.tsx
+++ app/AdminContainer/BACAdmin copy/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { DataTable } from '@/components/ui/data-table';
-import { StudentAffairscolumns } from '@/app/payments/StudentAffairsColumns';
+import { BACcolumns } from '@/app/payments/BACColumns';
 import GradualSpacing from '@/components/ui/gradual-spacing';
 import { HoverImageLinksData } from '@/components/ui/HoverLinkComponent';
 
@@ -11,23 +11,23 @@ export default function DemoPage() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch('/api/studentAffairs');
+        const res = await fetch('/api/bac');
         if (!res.ok) {
           throw new Error(`API call failed with status: ${res.status} ${res.statusText}`);
         }
         const jsonResponse = await res.json();
         console.log('Raw JSON Response:', jsonResponse);
 
-        if (!jsonResponse.hasOwnProperty('studentAffairs')) {
-          throw new Error('Expected property studentAffair not found in the response');
+        if (!jsonResponse.hasOwnProperty('bacs')) {
+          throw new Error('Expected property bac not found in the response');
         }
 
-        const { studentAffairs } = jsonResponse;
-        console.log('This is BAC Info Admin', studentAffairs);
+        const { bacs } = jsonResponse;
+        console.log('This is BAC Info Admin', bacs);
 
-        console.log('this is new data', studentAffairs);
+        console.log('this is new data', bacs);
 
-        setData(studentAffairs);
+        setData(bacs);
       } catch (error) {
         console.error('Failed to fetch data:', error);
       }
@@ -45,11 +45,11 @@ export default function DemoPage() {
   return (
     <div className='container mx-auto h-auto  flex flex-col gap-5   py-10'>
       <div className='w-full  '>
-        <GradualSpacing text='Student Affairs Admin' className='text-center text-6xl' />
+        <GradualSpacing text='Bac Admin' className='text-center text-6xl' />
       </div>
 
       <div className=''>
-        <DataTable columns={StudentAffairscolumns} data={data} />
+        <DataTable columns={BACcolumns} data={data} />
       </div>
 
       <div className='w-full    '>
