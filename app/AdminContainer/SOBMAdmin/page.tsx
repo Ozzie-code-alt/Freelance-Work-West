17153cb750d732c67824a2e6de204aa32ea0e4f2
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-11-03 18:04:59 +0800

    feat: add SOBM admin

--- app/AdminContainer/SOBMAdmin/page.tsx
+++ app/AdminContainer/SOBMAdmin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { DataTable } from '@/components/ui/data-table';
-import { Securitycolumns } from '@/app/payments/SecurityColumn';
+import { SOBMcolumns } from '@/app/payments/SOBMColumns';
 import GradualSpacing from '@/components/ui/gradual-spacing';
 import { HoverImageLinksData } from '@/components/ui/HoverLinkComponent';
 
@@ -11,23 +11,23 @@ export default function DemoPage() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch('/api/security');
+        const res = await fetch('/api/sobm');
         if (!res.ok) {
           throw new Error(`API call failed with status: ${res.status} ${res.statusText}`);
         }
         const jsonResponse = await res.json();
         console.log('Raw JSON Response:', jsonResponse);
 
-        if (!jsonResponse.hasOwnProperty('securities')) {
-          throw new Error('Expected property security not found in the response');
+        if (!jsonResponse.hasOwnProperty('sobms')) {
+          throw new Error('Expected property sobm not found in the response');
         }
 
-        const { securities } = jsonResponse;
-        console.log('This is BAC Info Admin', securities);
+        const { sobms } = jsonResponse;
+        console.log('This is BAC Info Admin', sobms);
 
-        console.log('this is new data', securities);
+        console.log('this is new data', sobms);
 
-        setData(securities);
+        setData(sobms);
       } catch (error) {
         console.error('Failed to fetch data:', error);
       }
@@ -45,11 +45,11 @@ export default function DemoPage() {
   return (
     <div className='container mx-auto h-auto  flex flex-col gap-5   py-10'>
       <div className='w-full  '>
-        <GradualSpacing text='Security Admin' className='text-center text-6xl' />
+        <GradualSpacing text='SOBM Admin' className='text-center text-6xl' />
       </div>
 
       <div className=''>
-        <DataTable columns={Securitycolumns} data={data} />
+        <DataTable columns={SOBMcolumns} data={data} />
       </div>
 
       <div className='w-full    '>
