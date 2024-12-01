dab02b6d3395a7dc1f064016c0b19005b0f51f31
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-12-02 05:52:42 +0800

    refactor: some changes

--- app/AdminPageHome/page.tsx
+++ app/AdminPageHome/page.tsx
@@ -1,11 +1,21 @@
+'use client';
 import { FocusCards } from '@/components/ui/focus-cards';
 import GradualSpacing from '@/components/ui/gradual-spacing';
 import { cards } from '@/constants/indes';
-
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 const AdminHome = () => {
-
+  const router = useRouter();
   return (
     <div className=' flex flex-col gap-10'>
+      <Button
+        onClick={() => {
+          router.push('/OfficePage');
+        }}
+        className=' ml-10 mt-10 w-[100px]'
+      >
+        Back
+      </Button>
       <div className='pt-10'>
         <GradualSpacing text='Admin Page' />
       </div>
